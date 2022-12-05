284c1d23e5638b063259f841186c0312398ee237
Fab <fabiomiguelchenhuang@gmail.com>
2022-12-05 18:48:03 +0000

    added some styling for home page

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,6 +1,17 @@
 import React from "react";
 import { useEffect, useState } from "react";
 
+import "./Home.scss";
+
+// Material UI Components
+import Box from "@mui/material/Box";
+import Input from "@mui/material/Input";
+import InputAdornment from "@mui/material/InputAdornment";
+import FormControl from "@mui/material/FormControl";
+import AccountCircle from "@mui/icons-material/AccountCircle";
+import Typography from "@mui/material/Typography";
+import Button from "@mui/material/Button";
+
 import Gun from "gun"; // eslint-disable-line no-use-before-define
 
 const gun = Gun();
@@ -16,26 +27,29 @@ const Home = () => {
 
   // How do we know which informations a peer is storing????
   useEffect(() => {
-
-
     // users.set(user);
 
     // user.get('follows').set(user)
 
     // console.log(user.get('follows'))
 
-    console.log("init")
+    console.log("init");
 
     setPosts([]);
-    setFollows([])
+    setFollows([]);
 
-    gun.get('users').get(currUser)
+    gun
+      .get("users")
+      .get(currUser)
       .get("posts_timeline")
       .map()
       .once((post) => {
         // Initialize User Posts
         // console.log(post.content)
-        setCurrUserPosts((currUserPosts) => [...currUserPosts, {content: post.content, createdAt: post.createdAt, from: post.from}]);
+        setCurrUserPosts((currUserPosts) => [
+          ...currUserPosts,
+          { content: post.content, createdAt: post.createdAt, from: post.from },
+        ]);
       });
 
     // console.log(currUserPosts);
@@ -57,8 +71,6 @@ const Home = () => {
     //     //   });
     //   }); // Initialize User follows and Posts
 
-    
-
     // console.log(user);
 
     // messages.map().once(message=>{    // Syncs the local state with the states from the peers
@@ -71,10 +83,11 @@ const Home = () => {
     // })
   }, []);
 
-  function getAllPosts(){
-    return currUserPosts.concat(posts)
+  function getAllPosts() {
+    return currUserPosts.concat(posts).sort((b, a) => {
+      return new Date(a.createdAt) - new Date(b.createdAt);
+    });
   }
-
 
   // function getPostsFromUser(userAlias) {
   //   const postsTimeline = gun.get(userAlias).get('posts_timeline')
@@ -82,7 +95,7 @@ const Home = () => {
   // }
 
   function saveMessage(e) {
-    e.preventDefault()
+    e.preventDefault();
     // const messages = gun.get('messages')
     // console.log(messages)
     // messages.set({
@@ -95,9 +108,16 @@ const Home = () => {
     //     message: '',
     // })
 
-    const newPost = { from: currUser, content: postField, createdAt: Date.now() }
+    const newPost = {
+      from: currUser,
+      content: postField,
+      createdAt: Date.now(),
+    };
 
-    gun.get('users').get(currUser).get('posts_timeline')
+    gun
+      .get("users")
+      .get(currUser)
+      .get("posts_timeline")
       .get(`${currUser}_${newPost.createdAt}`)
       .put(newPost);
 
@@ -113,46 +133,76 @@ const Home = () => {
   //   setCurrUser(e.target.value)
   // }
 
-  function formatDate(miliseconds){
-    return `${new Date(miliseconds).toISOString().split('T')[0]} - ${new Date(miliseconds).toISOString().split('T')[1].split('Z')[0].split('.')[0]}`
+  function formatDate(miliseconds) {
+    return `${new Date(miliseconds).toISOString().split("T")[0]} - ${
+      new Date(miliseconds)
+        .toISOString()
+        .split("T")[1]
+        .split("Z")[0]
+        .split(".")[0]
+    }`;
   }
 
   return (
-    <div>
-      {/* <input onChange={onChangeUser} placeholder="Name" name="name" value={currUser}>
-        </input> */}
-      <input
-        onChange={onChange}
-        placeholder="Message"
-        name="message"
-        value={postField}
-      ></input>
-      <div>
-        Follows:
-        {follows.map((user) => (
-          <div key={user}>-{user}</div>
-        ))}
-      </div>
-      <button onClick={saveMessage}>Send Message</button>
-      {/* {   
-            
-            state.messages.map(message=>(
-                <div key={message.createdAt}>
-                    <h2>{message.message}</h2>
-                    <h3>From: {message.name}</h3>
-                    <h4>Created At: {message.createdAt}</h4>
-                </div>
-            ))
-        } */}
+    <Box className="homepage-container">
+      <Box className="left-side_bar"></Box>
 
-      {getAllPosts().map((post) => (
-        <div key={post.createdAt}>
-          <h2>{post.content}</h2>
-          <h3>From: {post.from}</h3>
-          <h4>Created At: {formatDate(post.createdAt)}</h4>
-        </div>
-      ))}
-    </div>
+      <Box
+        className="content-wrapper"
+      >
+        <Box className="send_form-container">
+          <Typography variant="h5">Home</Typography>
+          <Box className="send_form">
+            <FormControl variant="standard" fullWidth>
+              {/* <InputLabel htmlFor="input-with-icon-adornment">
+              With a start adornment
+            </InputLabel> */}
+              <Input
+                placeholder="What's happening?"
+                disableUnderline={true}
+                startAdornment={
+                  <InputAdornment position="start">
+                    <AccountCircle sx={{ height: "50px", width: "50px" }} />
+                  </InputAdornment>
+                }
+                onChange={onChange}
+                value={postField}
+                sx={{ fontSize: "1.8em" }}
+              />
+            </FormControl>
+          </Box>
+          <Box className="submit-btn_container">
+            <Button
+              variant="contained"
+              sx={{ borderRadius: "16px", textTransform: "none", backgroundColor: '#5CC4FF'}}
+              onClick={saveMessage}
+            >
+              Tweet
+            </Button>
+          </Box>
+        </Box>
+        <Box className="posts-wrapper">
+          {getAllPosts().map((post) => (
+            <Box key={post.createdAt} className="post_container">
+              <InputAdornment>
+                <AccountCircle sx={{ height: "100px", width: "100px" }} />
+              </InputAdornment>
+              <Box className="post-content">
+                <Box className="post-content-title">
+                  <Typography>{post.from}</Typography>
+                  <Typography>{formatDate(post.createdAt)}</Typography>
+                </Box>
+
+                <Typography>{post.content}</Typography>
+              </Box>
+            </Box>
+          ))}
+        </Box>
+      </Box>
+      <Box
+        className="right-side_bar"
+      ></Box>
+    </Box>
   );
 };
 
